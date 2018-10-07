b6f28baf81ba2d0baa73ca864efaa80fc2f50908
Christopher Rivera <christopher.rivera27@gmail.com>
2018-10-07 10:13:28 -0700

    adds changeImage function

--- javascript/app.js
+++ javascript/app.js
@@ -4,16 +4,10 @@ $(document).ready(function() {
     
     var responses = ["Sashay away and slay!", "If Britney can get through 2007, you can get through today.", "You're strong. You're a Kelly Clarkson song. You got this!", "Be savage NOT average!", "You do YOU gurl!"];
     
-    $('#submit').click(giveResponse);
-
     function changeImage() {
         var image = document.getElementById('card-img');
-        img.src = "../images/joyeaux_card_blank.png";
+        img.src = "joyeaux_card_blank.png";
     }
 
-    function giveResponse() {
-        changeImage();
-        var num = Math.floor(Math.random() * responses.length);
-        
-    }
+    $('#submit').click(changeImage);
 });
