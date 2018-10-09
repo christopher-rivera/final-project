c149aa901ffb5493c9441a94de1d80d69e8da0c2
Christopher Rivera <christopher.rivera27@gmail.com>
2018-10-08 22:10:51 -0700

    changeImage function starting to work properly

--- javascript/app.js
+++ javascript/app.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 
-    event.preventDefault();
     
     var responses = ["Sashay away and slay!", "If Britney can get through 2007, you can get through today.", "You're strong. You're a Kelly Clarkson song. You got this!", "Be savage NOT average!", "You do YOU gurl!"];
     
+    
+
     function changeImage() {
-        var image = document.getElementById('card-img');
-        img.src = "joyeaux_card_blank.png";
+        event.preventDefault();
+        $('#decorated').attr('src', '../images/joyeaux_card_blank.png');
+            var random = Math.floor(Math.random() * 5);
+        
             // for loop
-            $('img').parent.replace(responses[i]);
+            // $('img').parent.replace(responses[i]);
     }
 
     $('#submit').click(changeImage);
