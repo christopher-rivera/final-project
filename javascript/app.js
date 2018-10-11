94215043e540df9a126574b00db5e99ea5a94b27
Christopher Rivera <christopher.rivera27@gmail.com>
2018-10-11 12:22:48 -0700

    finalizes images and adds jQuery array for responses

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
 
+    var myArray = ['../images/joyeaux_card_1.png', '../images/joyeaux_card_2.png','../images/joyeaux_card_3.png', '../images/joyeaux_card_4.png', '../images/joyeaux_card_5.png']    
     
-    var responses = ["Sashay away and slay!", "If Britney can get through 2007, you can get through today.", "You're strong. You're a Kelly Clarkson song. You got this!", "Be savage NOT average!", "You do YOU gurl!"];
-    
-    
-
     function changeImage() {
         event.preventDefault();
-        $('#decorated').attr('src', '../images/joyeaux_card_blank.png');
-            var random = Math.floor(Math.random() * 5);
-        
-            // for loop
-            // $('img').parent.replace(responses[i]);
+        var num = Math.floor(Math.random() * myArray.length);
+        $('#decorated').attr('src', myArray[num]);
     }
 
     $('#submit').click(changeImage);
+    
 });
